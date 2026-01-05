c6b9d7e888f80306d30dfa159124ba5e764818f1
MKatay <kmagdi@kkando.hu>
2026-01-05 13:51:25 +0100

    env variables

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
+      
     }),
+    
   ],
+  build: {
+        chunkSizeWarningLimit: 1600
+    }
 })
